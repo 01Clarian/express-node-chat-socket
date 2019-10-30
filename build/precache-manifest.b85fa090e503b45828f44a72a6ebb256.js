61424e00a0ce284e262017ee3753616db14cb044
01Clarian <clarionnorth@gmail.com>
2019-10-30 15:28:54 -0400

    second commit

---
+++ build/precache-manifest.b85fa090e503b45828f44a72a6ebb256.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b22adb2c8b20702a19641381acab64a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a807f91d3182677503c",
+    "url": "/static/css/main.adca7637.chunk.css"
+  },
+  {
+    "revision": "dcc55a2f773f758d2e1a",
+    "url": "/static/js/2.540ae8ef.chunk.js"
+  },
+  {
+    "revision": "2a807f91d3182677503c",
+    "url": "/static/js/main.dd1b0e48.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
